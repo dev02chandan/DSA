55165b65235bfd6315b3060d5af97ac7110d17d0
dev02chandan <dev02chandan@gmail.com>
2023-05-20 10:25:29 +0530

    better solution Next Permutation

--- nextPermutation.py
+++ nextPermutation.py
@@ -1,25 +1,20 @@
 def nextPermutation(permutation, n):
-    final_list = []
     j = -1
-    for i in range(n-1, -1, -1):
+    for i in range(len(permutation)-1, -1, -1):
         if permutation[i] > permutation[i-1]:
             j = i-1
             break
     if j == -1:
-        return sorted(permutation)
+        permutation = permutation[::-1]
 
-    final_list = permutation[:j]
-    new_list = permutation[j:]
-    for i in range(len(new_list)-1, -1, -1):
-        if new_list[i] > permutation[j]:
-            k = i
-            final_list.append(new_list[k])
-            del new_list[i]
-            break
+    else:
+        for k in range(len(permutation)-1, j, -1):
+            if permutation[k] > permutation[j]:
+                permutation[k], permutation[j] = permutation[j], permutation[k]
+                break
+        permutation[j+1:] = permutation[j+1:][::-1]
 
-    final_list = final_list + sorted(new_list)
-    return final_list
+    return permutation
 
 
-permutation = [1, 3, 2]
-print(nextPermutation(permutation, len(permutation)))
+print(nextPermutation([1, 2, 3], 3))
