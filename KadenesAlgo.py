0be9ad667f416b0330e7111bb469df62ab7a2169
dev02chandan <dev02chandan@gmail.com>
2023-05-20 11:29:23 +0530

    Printing subarray

--- KadenesAlgo.py
+++ KadenesAlgo.py
@@ -2,15 +2,19 @@ def maxSubarraySum(arr, n):
     sum = 0
     max = float('-inf')
     for i in range(n):
+        if sum == 0:
+            start = i
         sum += arr[i]
         if max < sum:
             max = sum
+            arrStart = start
+            arrEnd = i
 
         if sum < 0:
             sum = 0
     if max < 0:
         max = 0
-    return max
+    return max, arr[arrStart: arrEnd+1]
 
 
 print(maxSubarraySum([1, 2, 7, -4, 3, 2, -10, 9, 1], 9))
