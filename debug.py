89860e6c49cf3795b28a4c83322b931d9a598e15
dev02chandan <dev02chandan@gmail.com>
2023-05-23 16:19:57 +0530

    Debug

--- debug.py
+++ debug.py
@@ -1,5 +1,2 @@
-matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-for i in range(len(matrix)):
-    for j in range(len(matrix)-1, i, -1):
-        matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-print(matrix)
+a = [1, 2]
+print(a[2:])
