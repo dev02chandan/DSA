56b794de71cf07626e3047dc232a31c1f827ad26
dev02chandan <dev02chandan@gmail.com>
2023-05-25 12:44:21 +0530

    Search Matrix Optimal 2D Binary Search

--- Search2DMat.py
+++ Search2DMat.py
@@ -1,36 +1,36 @@
-def searchRow(arr, low, high, target):
-
-    while (low <= high):
-
-        mid = low + (high - low)//2
-
-        if arr[mid] == target:
-            return True
-
-        elif arr[mid] < target:
-            low = mid+1
-
-        else:
-            high = mid-1
-
-    return False
-
-
 def searchMatrix(mat, target: int) -> bool:
-    i = 0
-    j = len(mat[0])-1
-    while (True):
-        if i == len(mat):
-            return False
-        if mat[i][j] == target:
+    # Using Imaginary Indices
+    # Row = Imaginary // 2
+    # Col = Imaginary % 2
+
+    # Set the low and high indices for binary search
+    low = 0
+    high = len(mat) * len(mat[0]) - 1
+
+    # Perform binary search until low <= high
+    while low <= high:
+        # Calculate the middle index
+        mid = low + (high - low) // 2
+
+        # Retrieve the element at the corresponding row and column
+        element = mat[mid // len(mat[0])][mid % len(mat[0])]
+
+        # If the target is found, return True
+        if element == target:
             return True
-        elif mat[i][j] > target:
-            return searchRow(arr=mat[i], low=0, high=j, target=target)
+
+        # If the element is less than the target, move the low index to the right
+        elif element < target:
+            low = mid + 1
+
+        # If the element is greater than the target, move the high index to the left
         else:
-            i += 1
+            high = mid - 1
+
+    # If the target is not found, return False
+    return False
+# Test Cases
 
 
-print(searchMatrix([[1, 2, 3, 4],
-                    [5, 6, 7, 8],
-                    [9, 10, 11, 12]], 8)
-      )
+print(searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 3))
+print(searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 2))
