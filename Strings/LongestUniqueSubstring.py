f41ad0e16e56270cbf539feaea39f8bbdf368074
dev02chandan <dev02chandan@gmail.com>
2023-06-09 14:57:26 +0530

    Optimal Solution.

--- Strings/LongestUniqueSubstring.py
+++ Strings/LongestUniqueSubstring.py
@@ -5,54 +5,36 @@ from math import *
 
 
 def uniqueSubstrings(input):
-
     # Two pointer approach
+    # Optimizing time to N instead of 2N
 
-    myset = set()  # Set to store unique characters
+    mapp = {}  # To store the index of the last occurrence of each character
     right = 0  # Right pointer
-    left = 0  # Left pointer
-    length = 0  # Length of current substring
+    left = 0   # Left pointer
+    length = 0  # Length of the longest substring with most different characters
 
-    while (right < len(input)):
+    while right < len(input):
+        if input[right] not in mapp:  # If the character is encountered for the first time
+            mapp[input[right]] = right  # Store the index of the character
+            length = max(length, right - left + 1)  # Update the length
+            right += 1
 
-        if input[right] not in myset:
-            myset.add(input[right])  # Add current character to the set
-            # Update the length if needed
-            length = max(length, right - left + 1)
-            right += 1  # Move the right pointer
-
-        else:
-            while (input[left] != input[right]):
-                # Remove characters until the first occurrence of the current character
-                myset.discard(input[left])
-                left += 1  # Move the left pointer
-
-            # Remove the first occurrence of the current character
-            myset.discard(input[left])
-            left += 1  # Move the left pointer
-
-            myset.add(input[right])  # Add current character to the set
-            right += 1  # Move the right pointer
+        else:  # If the character is already present in the map
+            # Update the left pointer to skip the previous occurrence
+            left = max(left, mapp[input[right]] + 1)
+            mapp[input[right]] = right  # Update the index of the character
+            length = max(length, right - left + 1)  # Update the length
+            right += 1
 
     return length
 
 
-# Test Cases:
-
-# Case 1:
-input1 = "abacabad"  # Input string
-# Expected output: 3
-# Explanation: The unique substrings are "aba", "bac", and "bad".
-print(uniqueSubstrings(input1))
-
-# Case 2:
-input2 = "abcdabcd"  # Input string
-# Expected output: 4
-# Explanation: The unique substrings are "abcd", "bcda", "cdab", and "dabc".
-print(uniqueSubstrings(input2))
-
-# Case 3:
-input3 = "aaaaa"  # Input string
-# Expected output: 1
-# Explanation: All characters are the same, so the only unique substring is the whole string.
-print(uniqueSubstrings(input3))
+# Test Cases
+# Expected output: 3, Longest substring with most different characters: "abc"
+print(uniqueSubstrings("aabccbb"))
+# Expected output: 2, Longest substring with most different characters: "ab"
+print(uniqueSubstrings("abbbb"))
+# Expected output: 6, Longest substring with most different characters: "abcdef"
+print(uniqueSubstrings("abcdef"))
+# Expected output: 3, Longest substring with most different characters: "cde"
+print(uniqueSubstrings("abccde"))
