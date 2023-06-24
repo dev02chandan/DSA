55397dd4047441b7845995cc072b62da31aa2b43
dev02chandan <dev02chandan@gmail.com>
2023-06-24 13:18:30 +0530

    Another simple approach (striver)

--- Linked Lists/11RotateLinkedList.py
+++ Linked Lists/11RotateLinkedList.py
@@ -1,15 +1,4 @@
-'''
-Approach:
-
-First, we calculate the length of the linked list using the length() function. This helps us determine the effective rotation position if k is greater than the length of the linked list.
-If k is greater than the length, we use the modulus operator to reduce it to an equivalent smaller value.
-If k is 0 or the linked list is empty, we return the head as it is.
-Next, we find the position at which we need to rotate the linked list. This position is calculated as count - k, where count is the length of the linked list.
-We iterate through the linked list to find the new head node by moving rotate_pos positions from the current head.
-We update the connections to rotate the linked list. We set the next pointer of the last node to the current head, and the next pointer of the node before the new head to None.
-Finally, we return the new head node of the rotated linked list.
-'''
-
+# Another Approach (Striver)
 
 class Node:
     def __init__(self, val, next=None):
@@ -17,114 +6,89 @@ class Node:
         self.next = next
 
 
-def print_linked_list(head):
-    """
-    Print the elements of a linked list.
-
-    Args:
-        head: Head node of the linked list.
-
-    """
-    curr = head
-    while curr:
-        print(curr.data, end=" -> ")
-        curr = curr.next
-    print("None")
-
-
-def length(head):
-    """
-    Calculate the length of the linked list.
-
-    Args:
-        head: Head node of the linked list.
-
-    Returns:
-        The length of the linked list.
-    """
-    count = 0
-    while head and head.next:
-        head = head.next
-        count += 1
-
-    return count + 1
-
-
 def rotate(head: Node, k: int) -> Node:
-    """
-    Rotate a linked list by k positions.
-
-    Args:
-        head: Head node of the linked list.
-        k: Number of positions to rotate.
-
-    Returns:
-        The head node of the rotated linked list.
-
-    """
+    # If k is 0 or head is None, no rotation needed
     if k == 0 or head is None:
         return head
 
-    count = length(head)
+    slow = head
+    count = 1
 
-    if k > count:
+    # Get the length of the linked list
+    while slow.next is not None:
+        slow = slow.next
+        count += 1
+
+    # If k is greater than or equal to the count, take modulus to get effective rotation
+    if k >= count:
         k = k % count
 
-    if k == 0:
-        return head
+    # Make the linked list circular by connecting the last node with the head
+    slow.next = head
 
-    # Find the position to rotate
-    rotate_pos = count - k
+    # Move slow to the (count - k)th node
+    for i in range(count - k):
+        slow = slow.next
 
-    # Find the new head
-    prev = None
-    curr = head
-    pos = 0
-    while curr and pos < rotate_pos:
-        prev = curr
-        curr = curr.next
-        pos += 1
+    # Break the circular link to make slow the new tail of the rotated list
+    nex = slow.next
+    slow.next = None
 
-    if prev:
-        prev.next = None
+    return nex
 
-    new_head = curr
-
-    # Update connections
-    while curr.next:
-        curr = curr.next
-
-    curr.next = head
-
-    return new_head
+# Approach:
+# 1. If k is 0 or the head is None, there is nothing to rotate, so return the head as it is.
+# 2. Traverse the linked list to find its length and keep track of the last node (slow) and the count.
+# 3. If k is greater than or equal to the count, take the modulus of k with count to get the effective rotation.
+# 4. Make the linked list circular by connecting the last node (slow) with the head.
+# 5. Move slow to the (count - k)th node, which will be the new last node after rotation.
+# 6. Break the circular link by setting slow.next to None, making it the new tail of the rotated linked list.
+# 7. Return the next node after slow as the new head of the rotated linked list.
+# Time complexity: O(N) where N is the length of the linked list.
+# Space complexity: O(1)
 
 
-# Test Case 1: Rotate a linked list by 2 positions
+# Testcases
+# 1 -> 2 -> 3 -> 4 -> 5 -> None
 head = Node(1, Node(2, Node(3, Node(4, Node(5)))))
 k = 2
 rotated_head = rotate(head, k)
-# Expected output: 4 -> 5 -> 1 -> 2 -> 3
-# The linked list is rotated by 2 positions, resulting in a new head with value 4.
-# The new list is 4 -> 5 -> 1 -> 2 -> 3.
-print_linked_list(rotated_head)  # Helper function to print the linked list
+print(rotated_head.data, end=" -> ")  # Output: 4 -> 5 -> 1 -> 2 -> 3 -> None
+print(rotated_head.next.data, end=" -> ")
+print(rotated_head.next.next.data, end=" -> ")
+print(rotated_head.next.next.next.data, end=" -> ")
+print(rotated_head.next.next.next.next.data, end=" -> ")
+print(rotated_head.next.next.next.next.next)
 
-# Test Case 2: Rotate a linked list by 0 positions
+# 1 -> 2 -> 3 -> 4 -> 5 -> None
 head = Node(1, Node(2, Node(3, Node(4, Node(5)))))
 k = 0
 rotated_head = rotate(head, k)
-# Expected output: 1 -> 2 -> 3 -> 4 -> 5
-# Since k is 0, the linked list remains unchanged.
-# The new list is the same as the original list.
-print_linked_list(rotated_head)
+print(rotated_head.data, end=" -> ")  # Output: 1 -> 2 -> 3 -> 4 -> 5 -> None
+print(rotated_head.next.data, end=" -> ")
+print(rotated_head.next.next.data, end=" -> ")
+print(rotated_head.next.next.next.data, end=" -> ")
+print(rotated_head.next.next.next.next.data, end=" -> ")
+print(rotated_head.next.next.next.next.next)
 
-# Test Case 3: Rotate an empty linked list
-head = None
+# 1 -> 2 -> 3 -> 4 -> 5 -> None
+head = Node(1, Node(2, Node(3, Node(4, Node(5)))))
+k = 8
+rotated_head = rotate(head, k)
+print(rotated_head.data, end=" -> ")  # Output: 3 -> 4 -> 5 -> 1 -> 2 -> None
+print(rotated_head.next.data, end=" -> ")
+print(rotated_head.next.next.data, end=" -> ")
+print(rotated_head.next.next.next.data, end=" -> ")
+print(rotated_head.next.next.next.next.data, end=" -> ")
+print(rotated_head.next.next.next.next.next)
+
+head = Node(1)  # 1 -> None
 k = 3
 rotated_head = rotate(head, k)
-# Expected output: None
-# The linked list is empty, so there is no rotation possible.
-# The output should be None.
-print_linked_list(rotated_head)
+print(rotated_head.data, end=" -> ")  # Output: 1 -> None
+print(rotated_head.next)
 
-
-# Edge Case = when k==count
+head = None  # None
+k = 5
+rotated_head = rotate(head, k)
+print(rotated_head)  # Output: None
