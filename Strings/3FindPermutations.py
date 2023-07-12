365ce7204b9135a12f207033ddd0e52612323470
dev02chandan <dev02chandan@gmail.com>
2023-07-12 15:44:20 +0530

    Striver Method

--- Strings/3FindPermutations.py
+++ Strings/3FindPermutations.py
@@ -1,17 +1,20 @@
 def printPermutations(s):
-    nums = list(s)
-    prefix = []
-    result = []
+    arr = list(s)
+    ans = []
 
-    def recursive(prefix, nums):
-        if len(nums) == 0:
-            result.append(prefix)
+    def recursion(index, arr):
+        if index == len(arr):
+            ans.append(''.join(arr))
+            return
 
-        for i in range(len(nums)):
-            recursive(prefix + [nums[i]], nums[:i]+nums[i+1:])
+        for i in range(index, len(arr)):
+            arr[index], arr[i] = arr[i], arr[index]
+            recursion(index + 1, arr)
+            # Backtrack, restore original state
+            arr[index], arr[i] = arr[i], arr[index]
 
-    recursive(prefix, nums)
-    return result
+    recursion(0, arr)
+    return ans
 
 
-print(printPermutations("aca"))
+print(printPermutations("abc"))
