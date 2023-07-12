a5fb50b0cff9986816b19b57789bd9c9ac57e054
dev02chandan <dev02chandan@gmail.com>
2023-07-12 15:44:30 +0530

    testing

--- debug.py
+++ debug.py
@@ -1,12 +1,2 @@
-def isPallindrome(string, start, end):
-    while start <= end:
-        if string[start] != string[end]:
-            return False
-        start += 1
-        end -= 1
-    return True
-
-
 string = "abc"
-print(string)
-print(isPallindrome(string, 0, 0))
+print(list(string))
