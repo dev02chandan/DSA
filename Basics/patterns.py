da5820110f269772c057fa3aa010e619cfbb3fdb
dev02chandan <dev02chandan@gmail.com>
2024-05-30 16:37:17 +0530

    Diamond Star pattern

--- Basics/patterns.py
+++ Basics/patterns.py
@@ -128,3 +128,33 @@ for i in range(n):
 #  ***   i=1  spaces=1 => stars = n-i
 #   *    i=2  spaces=2
 '''
+
+n = 4
+
+for i in range(n):
+    for j in range(n-i-1):
+        print(' ',end='')
+    for j in range(2*i+1):
+        print('*', end='')
+    for j in range(n-i-1):
+        print(' ', end='')
+    print()
+
+for i in range(n):
+    for j in range(i):
+        print(' ', end='')
+    for j in range(2*(n-i)-1):
+        print('*', end='')
+    for j in range(i):
+        print(' ', end='')
+    print()
+
+# Mixture of the last two patterns
+#    *   
+#   ***  
+#  ***** 
+# *******
+# *******
+#  ***** 
+#   ***  
+#    *   
