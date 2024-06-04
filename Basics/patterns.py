4274913f53f22d776cf5f7750f3587596ce06298
dev02chandan <dev02chandan@gmail.com>
2024-06-04 10:47:50 +0530

    patterns

--- Basics/patterns.py
+++ Basics/patterns.py
@@ -217,3 +217,53 @@ for i in range(n-1, 0, -1):
 1 0 1 0 1 
 '''
 
+'''
+n = 5
+num = 1
+for i in range(n):
+    for j in range(i+1):
+        print(num, end='\t') 
+        num += 1
+    print()
+
+
+
+1
+2 3
+4 5 6
+
+1
+2       3
+4       5       6
+7       8       9       10
+11      12      13      14      15
+
+
+'''
+
+'''
+n = 5
+for i in range(n):
+    for j in range(2*n):
+        if j>i and j<2*n-i-1:
+            print(' ', end='')
+        elif j<=i:
+            print(j+1, end='')
+        else:
+            print(2*n-j, end='')
+    print()
+
+
+
+1    1          i=0     j=0,  1,2,3,4,  5
+12  21
+123321
+
+1        1
+12      21
+123    321
+1234  4321
+1234554321
+
+
+'''
