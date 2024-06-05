bfa370a347ebc5c921f30b9cd86f8f381e8c3269
dev02chandan <dev02chandan@gmail.com>
2024-06-05 11:43:06 +0530

    Leetcode solution

--- Basics/reverse.py
+++ Basics/reverse.py
@@ -1,4 +1,4 @@
-num = 12340
+num = -12340
 # print(int(str(num)[::-1]))
 
 def reversed(num):
@@ -12,3 +12,28 @@ def reversed(num):
 
 print(reversed(num))
 
+
+
+# Leet code
+
+'''
+class Solution:
+    def reverse(self, x: int) -> int:
+        revNum = 0
+        num = abs(x)
+
+        while True:
+            revNum = revNum * 10 + num % 10
+            num = num//10
+            if num == 0:
+                break
+
+        if revNum > 2**31 or revNum < 2**(-31):
+            return 0
+
+        if x < 0: 
+            return -(revNum)
+        else:
+            return revNum
+
+'''
